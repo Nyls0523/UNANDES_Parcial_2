b3a0328565e869fb115935b6e4f2ebfd7cc73831
Nyls0523 <slynvilela@gmail.com>
2022-11-15 10:40:26 -0400

    cambio 1

---
+++ WebApplication_P2/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication_P2.Data.Entity;
+
+namespace WebApplication_P2.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Planta> Plantas { get; set; } = null!;
+        public DbSet<Riego> riegos { get; set; } = null!;
+    }
+}
---
+++ WebApplication_P2/Data/Entity/Riego.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApplication_P2.Data.Entity
+{
+    public class Riego
+    {
+    }
+}
